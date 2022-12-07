092cb0f7e4d867f14f8a048024996970650f7f0b
Baturpcu <batusalkim11@gmail.com>
2022-12-07 19:22:43 +0300

    update weather-app 07.12.2022
    update weather-app 07.12.2022

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useEffect } from "react";
 import './App.css';
 
 function App() {
+  const key = "359eec33cd1498993aed25e9eed9406d";
+  useEffect(() => {
+    async function getApi(){
+      try{
+        const response = await axios.get("/user?ID=12345");
+        console.log(response);
+      }catch(error){
+        console.error(error);
+      }
+    }
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Batu
     </div>
   );
 }
