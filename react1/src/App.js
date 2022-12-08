20148bdf57f8ee9a52ee8cc38bb5b2e27e43262b
Baturpcu <batusalkim11@gmail.com>
2022-12-08 14:05:21 +0300

    update 08.12.2022-14:05
    update 08.12.2022-14:05

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,23 +1,38 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { useState } from "react";
 import './App.css';
+import City from "./City";
 
 function App() {
   const key = "359eec33cd1498993aed25e9eed9406d";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
   useEffect(() => {
     async function getApi(){
       try{
-        const response = await axios.get("/user?ID=12345");
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`);
         console.log(response);
+        setCity(response.data);
       }catch(error){
         console.error(error);
       }
     }
-  })
+    getApi();
+  },[search]);
+  console.log(search);
   
   return (
     <div className="App">
-      Batu
+      <div>
+          <input
+            onChange={(e) => setSearch(e.target.value)} 
+          type="text" placeholder="şehir girin"
+          className="px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring"
+        />
+        <City city = {city}></City>
+      </div>
+      
     </div>
   );
 }
---
+++ react1/src/City.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const City = ({city}) => {
+  return (
+    
+    <div>
+     hello
+    </div>
+   
+  )
+}
+
+export default City
